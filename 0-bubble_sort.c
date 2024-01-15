045aed33f46f50b2c24fae00bc5e8e5b6e963b36
Cedric002 <cedgnleblanc@gmail.com>
2024-01-15 06:54:02 -0800

    correction v1.2 du fichier 0-bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,28 +8,25 @@
  * Return: sort in ascending order
  *
  */
-void bubble_sort(int *array, size_t size)
+void bubble_sort(int* array, size_t size)
 {
-	void bubble_sort(int* array, size_t size)
+	for (size_t i = 0; i < size - 1; i++)
 	{
-		for (size_t i = 0; i < size - 1; i++)
+		for (size_t j = 0; j < size - i - 1; j++)
 		{
-			for (size_t j = 0; j < size - i - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				printf("After swap: ");
+
+				for (size_t k = 0; k < size; k++)
 				{
-					int temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-
-					printf("After swap: ");
-
-					for (size_t k = 0; k < size; k++)
-					{
-						printf("%d ", array[k]);
-					}
-					printf("\n");
+					printf("%d ", array[k]);
 				}
+				printf("\n");
 			}
 		}
 	}
